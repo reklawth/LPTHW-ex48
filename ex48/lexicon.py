b26527981ed31580f4caaae9abc7a2420149f1aa
Thomas Walker <thwalker@Independence.fios-router.home>
2016-12-11 15:46:43 -0500

    first commit

---
+++ ex48/lexicon.py
@@ -0,0 +1,25 @@
+
+def directions(val):
+	words = val.split()
+	
+def verbs(val):
+	words = val.split()
+	
+def stops(val):
+	words = val.split()
+	
+def nouns(val):
+	words = val.split()
+
+def numbers(val):
+	words = val.split()
+	
+def errors(val):
+	words = val.split()
+
+
+
+print("Please provide a sentence:")
+sentence = raw_input('> ')
+words = sentence.split()
+print words
