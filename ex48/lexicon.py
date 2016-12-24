5898c64d8f64d7be31ce321b8f94806565f8ee10
Thomas Walker <thwalker@Independence.fios-router.home>
2016-12-24 00:06:05 -0500

    revising Lexicon.py file

--- ex48/lexicon.py
+++ ex48/lexicon.py
@@ -1,6 +1,25 @@
+'''
+This purpose of this program is to provide a scanner, that outputs a tuple to user defined
+input.  The tuple is created by matching defined lists of words with similar characteristics
+against the user input.
+
+This program should utilize Tuples and works with Python 2.7
+'''
+
+# Lexicon: Defined vocabulary of words to be used for this package
+direction_words = ['north','south','east','west','down','up','left','right''back']
+verbs = ['go','stop','kill','eat']
+stop_words = ['the','in','of','from','at','it']
+nouns = ['door','bear','princess','cabinet']
+
+def scan(sentence):
+	words = sentence.split()
+	print('in scan method ' + str(words))
 
 def directions(val):
-	words = val.split()
+	sentence = val
+	scan(val)
+	
 	
 def verbs(val):
 	words = val.split()
@@ -16,10 +35,9 @@ def numbers(val):
 	
 def errors(val):
 	words = val.split()
-
-
+	
 
 print("Please provide a sentence:")
-sentence = raw_input('> ')
-words = sentence.split()
-print words
+sentence_line = raw_input('> ')
+scan(sentence_line)
+print("past")
